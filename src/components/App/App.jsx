7f71a341366211c9db5866ed4ea0ebd084b01987
AH400796 <a.hokhman@gmail.com>
2023-05-11 21:16:32 +0300

    111

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -31,7 +31,6 @@ export default function App() {
     setIsRefreshing(true);
     if (token === null) {
       setUserEmail('');
-
       setIsLoggedIn(false);
       localStorage.removeItem('loggedin');
       setIsRefreshing(false);
